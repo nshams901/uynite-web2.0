117c4fce3911beb1681cfcc2770862072a396c7b
nshams <nehalshams786@gmail.com>
2023-06-21 17:05:22 +0530

    mobile number registration

--- src/Components/Login/Content/EnterCode/SignupOtp.jsx
+++ src/Components/Login/Content/EnterCode/SignupOtp.jsx
@@ -89,12 +89,18 @@ const SignupOtp = ({ title }) => {
       confirmationResult
         .confirm(otp)
         .then((result) => {
+          const payload = {
+            password: signupData.password,
+            datetime: Date.now().toString(),
+            uemail: signupData.uemail
+          }
           dispatch(settingOtp(""));
           setState({ ...state, showModal: true });
           // User signed in successfully.
           const user = result.user;
           setIsLoading(false);
           dispatch(isOtpValid({ validOtp: true, userInfo: false }));
+          dispatch(userRegistration(payload))
           navigate(`/auth/verification/signup?${userInfo?.profileType}`);
         })
         .catch((error) => {
